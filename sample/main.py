cd066ad8547549b3da927c201e0f12b76b4c624a
xryuseix <ryusei143.shootingstar@gmail.com>
2020-10-29 11:44:38 +0900

    fix broken image

--- sample/main.py
+++ sample/main.py
@@ -2,16 +2,20 @@ import zoomg
 import cv2
 import sys
 import numpy
-
+from tqdm import tqdm
 
 # ビデオ読み込みの初期設定
 filename = sys.argv[1]
 video = cv2.VideoCapture(filename)
 height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
 # zoomgの初期化
-zoom = zoomg.Zoomg(width, height)
+zoom = zoomg.Zoomg(height, width)
+
+# 進捗バー
+bar = tqdm(total=count)
 
 while True:
     # フレーム読み込み
@@ -20,10 +24,14 @@ while True:
         break
     # zoomgに画像を追加
     zoom.add_image(frame)
+    # 進捗バー更新
+    bar.update(1)
 
 # 背景画像を生成
 zoom.generate_image()
 # 背景画像を取得
 image = zoom.get_image()
 
+# 背景画像を保存
 cv2.imwrite("room.png", numpy.array(image))
+bar.close()
