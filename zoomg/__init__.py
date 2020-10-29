662aa7f3316a5430ee116403fe47691b0c7c7caf
xryuseix <ryusei143.shootingstar@gmail.com>
2020-10-29 11:45:05 +0900

    fix url, version

--- zoomg/__init__.py
+++ zoomg/__init__.py
@@ -3,8 +3,7 @@ from . import zoomg
 __copyright__ = "Copyright (C) 2020 Ryusei Ishikawa, Satoki Tsuji"
 __license__ = "MIT"
 __author__ = "Ryusei Ishikawa, Satoki Tsuji"
-__url__ = "https://github.com/xryuseix/roomg"
-__version__ = "1.0.5"
+__url__ = "https://github.com/Tsuku43/zoomg"
 
 __all__ = ["Zoomg"]
 
