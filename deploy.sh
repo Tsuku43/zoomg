f589b673c1a8ce6837765a91a02604c2e53f48ee
xryuseix <ryusei143.shootingstar@gmail.com>
2021-05-28 23:34:01 +0900

    fix: deplayスクリプトにpyenvコマンドを追加

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,10 @@ rm -rf zoomg.egg-info/* dist/*
 python setup.py sdist
 python setup.py bdist_wheel
 
+pyenv local 3.9.1 # twineが3.6.4では動かない
 twine upload --repository testpypi dist/* # テスト
 # pip --no-cache-dir install --upgrade --index-url https://test.pypi.org/simple/ zoomg # テストダウンロード
 
 # twine upload --repository pypi dist/* # 本番
 # pip --no-cache-dir install --upgrade zoomg # 本番ダウンロード
+pyenv local 3.6.4
